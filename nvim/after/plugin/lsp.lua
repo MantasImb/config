1ad88a94bdbc018227932273dd8fa4f3175120c8
Mantas Imbrasas <mantas@Mantass-MacBook-Air.local>
2024-09-12 20:54:44 +0300

    change typescript language server in lsp

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,44 +1,44 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindings
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
+	-- see :help lsp-zero-keybindings
+	-- to learn the available actions
+	lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 -- to learn how to use mason.nvim
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guide/integrate-with-mason-nvim.md
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-	  'tsserver',
-	  'eslint',
-	  'astro',
-	  'rust_analyzer',
-	  'tailwindcss',
-	  'cssls',
-	  'dockerls',
-	  'html',
-	  'jsonls',
-	  'lua_ls',
-	  'pylsp',
-	  'taplo',
-	  'sqlls',
-	  'bashls',
-  },
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-  },
+	ensure_installed = {
+		'ts_ls',
+		'eslint',
+		'astro',
+		'rust_analyzer',
+		'tailwindcss',
+		'cssls',
+		'dockerls',
+		'html',
+		'jsonls',
+		'lua_ls',
+		'pylsp',
+		'taplo',
+		'sqlls',
+		'bashls',
+	},
+	handlers = {
+		function(server_name)
+			require('lspconfig')[server_name].setup({})
+		end,
+	},
 })
 
-require'lspconfig'.sqlls.setup{
-  capabilities = capabilities,
-  filetypes = { 'sql' },
-  root_dir = function(_)
-    return vim.loop.cwd()
-  end,
+require 'lspconfig'.sqlls.setup {
+	capabilities = capabilities,
+	filetypes = { 'sql' },
+	root_dir = function(_)
+		return vim.loop.cwd()
+	end,
 }
 
 local cmp = require('cmp')
@@ -48,12 +48,12 @@ lsp_zero.set_preferences({
 	sign_icons = {}
 })
 
-lsp_zero.on_attach(function (client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-	vim.keymap.set("n", "gd", function () vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function () vim.lsp.buf.hover() end, opts)
+lsp_zero.on_attach(function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd" , function () vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
 	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
 	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
@@ -61,27 +61,27 @@ lsp_zero.on_attach(function (client, bufnr)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
 	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 	vim.keymap.set("n", "<leader>h", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, opts)
-	end)
+end)
 
 
 cmp.setup({
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    mapping = cmp.mapping.preset.insert({
-      ['<C-Space>'] = cmp.mapping.complete(),
-      ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-      ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-      ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    }),
-    snippet = {
-      expand = function(args)
-        require('luasnip').lsp_expand(args.body)
-      end,
-    },
-    -- sources = {
-    --   { name = "supermaven" },
-    -- },
-  })
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-f>'] = cmp_action.luasnip_jump_forward(),
+		['<C-b>'] = cmp_action.luasnip_jump_backward(),
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
+	}),
+	snippet = {
+		expand = function(args)
+			require('luasnip').lsp_expand(args.body)
+		end,
+	},
+	-- sources = {
+	--   { name = "supermaven" },
+	-- },
+})
